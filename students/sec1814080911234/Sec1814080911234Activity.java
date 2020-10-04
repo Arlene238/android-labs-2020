d6a8167dded04c4b13c92f869075a724539715aa
Arlene238 <71618067+Arlene238@users.noreply.github.com>
2020-10-04 21:57:32 +0800

    Revert "#1 #255 第1次实验"

--- students/sec1814080911234/Sec1814080911234Activity.java
+++
@@ -1,8 +0,0 @@
-package edu.hzuapps.androidlabs.sec1814080911234;
-
-public class Sec1814080911234Activity {
-
-    public static void main(String[] args) {
-        System.out.println("Are you ok");
-    }
-}
